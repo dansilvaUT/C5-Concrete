1490c0b94a84aa1d457680c896680f58004dded9
dansilvaUT <dan13silv@gmail.com>
2021-04-15 21:39:44 -0700

    initial set up

---
+++ src/Components/Header/Nav/Nav.js
@@ -0,0 +1,17 @@
+import Navbar from 'react-bootstrap/Navbar';
+
+const Nav = () => {
+    return (
+        <></>
+        // <Navbar bg="dark" variant="dark">
+        //     <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        //     <Nav className="mr-auto">
+        //         <Nav.Link href="#home">Home</Nav.Link>
+        //         <Nav.Link href="#features">Features</Nav.Link>
+        //         <Nav.Link href="#pricing">Pricing</Nav.Link>
+        //     </Nav>
+        // </Navbar>
+    )
+}
+
+export default Nav;
