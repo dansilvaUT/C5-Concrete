a5f5b2d8a3b3c1c8e8d487c73a015b294b7b6f13
dansilvaUT <dan13silv@gmail.com>
2021-04-19 22:04:59 -0700

    base styling started

--- src/Components/Header/Nav/NavBar.js
+++ src/Components/Header/Nav/NavBar.js
@@ -1,16 +1,17 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import './Nav.scss';
 
 const NavBar = () => {
     return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top" >
-            <Navbar.Brand href="#home">C5 Concrete</Navbar.Brand>
+        <Navbar className="navbar-component" collapseOnSelect expand="lg" variant="light" fixed="top" >
+            <Navbar.Brand href="#home" id="brand-name">C5 Concrete</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="#features">About</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
-                    <Nav.Link href="#pricing">Contact</Nav.Link>
+                    <Nav.Link className="nav-item" href="#features">About</Nav.Link>
+                    <Nav.Link className="nav-item" href="#pricing">Pricing</Nav.Link>
+                    <Nav.Link className="nav-item" href="#pricing">Contact</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
