59dea33d891721668f06417d84b4b9c1f22e2b33
dansilvaUT <dan13silv@gmail.com>
2021-04-20 21:47:11 -0700

    carousel added. Need to fix pictures

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,7 +1,9 @@
 import Container from 'react-bootstrap/Container';
-
-
+import Carousel from 'react-bootstrap/Carousel';
+import { cArr } from './pictureLoad';
+import logo from '../../assets/about-pic/about_1.jpeg';
 const About = () => {
+    console.log("dd", cArr)
     return (
         <Container>
             <h1>Who We Are</h1>
@@ -15,6 +17,25 @@ const About = () => {
                 pancetta ball tip corned beef. Kielbasa cow meatloaf,
                 sirloin alcatra capicola burgdoggen turkey. Cow pig frankfurter
                        kielbasa turkey.</p>
+                       <img src={logo}/>
+                <Carousel>
+
+                        {cArr.map(p => (
+                            <Carousel.Item interval={1000}>
+                                <img src={p.url} alt={p.alt}/>
+                            </Carousel.Item>
+                        ))}
+                        {/* <img
+                            className="d-block w-100"
+                            src=""
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption> */}
+                    
+                </Carousel>
             </Container>
         </Container>
     )
---
+++ src/Components/About/pictureLoad.js
@@ -0,0 +1,7 @@
+
+const aboutURL = '../../assets/about-pic/';
+const about1 = {url: `${aboutURL}about_1.jpeg`, alt: 'sidewalk'};
+const about2 = {url: `${aboutURL}about_2.jpeg`, alt: 'foundation'};
+const about3 = {url: `${aboutURL}about_3.jpeg`, alt: 'walk_way'};
+
+export const cArr = [about1, about2, about3];
