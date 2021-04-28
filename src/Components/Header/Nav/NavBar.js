3a0752d15b7e558587381558f8252b3699e1bb37
dansilvaUT <dan13silv@gmail.com>
2021-04-27 21:22:46 -0700

    class name added for navbutton

--- src/Components/Header/Nav/NavBar.js
+++ src/Components/Header/Nav/NavBar.js
@@ -7,7 +7,7 @@ const NavBar = () => {
         <Navbar className="navbar-component" collapseOnSelect expand="lg" variant="light" fixed="top" >
             <Navbar.Brand href="#home" id="brand-name">C5 Concrete</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
+            <Navbar.Collapse id="responsive-navbar-nav" className="toggle-button">
                 <Nav className="mr-auto">
                     <Nav.Link className="nav-item" href="#features">About</Nav.Link>
                     <Nav.Link className="nav-item" href="#pricing">Pricing</Nav.Link>
