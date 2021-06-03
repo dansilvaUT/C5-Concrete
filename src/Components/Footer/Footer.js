b682f34c4fc1d00772d140c4aa8810921f9ebafa
dansilvaUT <dan13silv@gmail.com>
2021-06-03 14:00:26 -0700

    responsive done

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import {faPhoneAlt} from '@fortawesome/free-solid-svg-icons';
 import './Footer.scss';
 
 const Footer = () => {
     return (
         <footer className="footer">
-            <p>&copy; Daniel Silva | C5</p>
+            <p>&copy; Daniel Silva | C5 | </p>
+            <a class="link facebook-link" href="https://www.facebook.com/C5ConcreteCompany" target="_blank" rel="noreferrer">
+                <FontAwesomeIcon className="facebook-icon" icon={faFacebook} />
+            </a>
+            <FontAwesomeIcon className="facebook-icon" icon={faPhoneAlt} />
+            <span id="phone">:702-501-7773</span>
         </footer>
     )
 }
