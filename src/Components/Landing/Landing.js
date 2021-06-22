e30b49375574ce0091eab0eb3eee2a7b91a71207
dansilvaUT <dan13silv@gmail.com>
2021-06-21 21:18:23 -0700

    landing and heading query

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
 import Heading from '../Fillers/Headings/Heading';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faWarehouse } from '@fortawesome/free-solid-svg-icons';
+import logo from '../../assets/c5_logo.jpeg'
 import './Landing.scss';
 
 const Landing = () => {
@@ -9,7 +9,7 @@ const Landing = () => {
     <>
       <Container fluid className="jumbotron-about" >
         <Heading classname="heading top-header" text='C5 Concrete' />
-        <FontAwesomeIcon className="warehouse-icon" icon={faWarehouse} />
+        <Image className="logo-img" src={logo} roundedCircle />
       </Container>
     </>
 
--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -4,10 +4,12 @@ import './List.scss';
 const List = () => {
     return (
         <ListGroup className="list-group-holder" variant="flush">
-            <ListGroup.Item>Concrete</ListGroup.Item>
-            <ListGroup.Item>Residential</ListGroup.Item>
-            <ListGroup.Item>Foundation</ListGroup.Item>
-            <ListGroup.Item>Patios</ListGroup.Item>
+            <span></span>
+            <ListGroup.Item className="list-item">Concrete</ListGroup.Item>
+            <ListGroup.Item className="list-item">Residential</ListGroup.Item>
+            <ListGroup.Item className="list-item">Foundation</ListGroup.Item>
+            <ListGroup.Item className="list-item">Patios</ListGroup.Item>
+            <span></span>
         </ListGroup>
     )
 }
